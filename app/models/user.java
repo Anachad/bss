573c01f9eb3e7fad100ca3dca07d633ba213dc03
Anachad <a.anachad@brams.net>
2013-08-02 10:23:36 +0000

    test

---
+++ app/models/user.java
@@ -0,0 +1,12 @@
+package models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Brams
+ * Date: 01/08/13
+ * Time: 14:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class user {
+
+}
